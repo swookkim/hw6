d8c2d3336a5036ab45abbaa357b93f2fb103991a
swookkim <sangwook.kim@kellogg.northwestern.edu>
2021-05-10 14:19:17 -0500

    1

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -60,7 +60,7 @@ exports.handler = async function(event) {
     // a lambda function returns a status code and a string of data
     return {
       statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: `Hello from the back-end!` // a string of data
+      body: JSON.stringify(returnValue) // a string of data
     }
   }
 }
